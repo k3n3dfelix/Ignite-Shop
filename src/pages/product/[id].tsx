92cfa75df3209186392d4037bea24a17d3eca011
Kened Silva Felix <ksfelix1@stefanini.com>
2023-03-10 14:14:28 -0300

    Feito integração de compra com Stripe

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -4,6 +4,8 @@ import { stripe } from "../../lib/stripe";
 import Stripe from "stripe";
 import { ImageContainer, ProductContainer, ProductDetails} from '../../styles/pages/product';
 import Image from "next/image";
+import axios from "axios";
+import { useState } from "react";
 
 interface ProductProps {
   product: {
@@ -12,14 +14,38 @@ interface ProductProps {
     imageUrl: string;
     price: string;
     description: string;
+    defaultPriceId: string;
   }
 }
 
 export default function Product({product}: ProductProps) {
   const { isFallback } = useRouter()
+  const [isCreatingCheckoutSession, setisCreatingCheckoutSession] = useState(false);
+  //const router = useRouter();
 
   if(isFallback){
     return <p>Loading...</p>
+  }
+
+  async function handleBuyProduct() {
+   try {
+
+    setisCreatingCheckoutSession(true);
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId,
+      })
+      //Redirecnionar dentro da aplicação
+      //router.push('/checkout');
+
+      const { checkoutUrl } = response.data;
+      window.location.href = checkoutUrl;
+    }catch(error){
+      //Conectar com uma ferramente de observabilidade (Dtadog / Sentry)
+      setisCreatingCheckoutSession(false);
+      alert('Falha ao redirecionar ao checkout!')
+    }finally{
+
+    }
   }
   return (
    <ProductContainer>
@@ -30,7 +56,7 @@ export default function Product({product}: ProductProps) {
         <h1>{product.name}</h1>
         <span>{product.price}</span>
         <p>{product.description}</p>
-        <button>Comprar Agora</button>
+        <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>Comprar Agora</button>
       </ProductDetails>
    </ProductContainer>
   )
@@ -69,7 +95,8 @@ export const getStaticProps: GetStaticProps<any, {id: string}> = async ({ params
           style: "currency",
           currency: "BRL",
         }).format(price.unit_amount! / 100),
-        description: product.description
+        description: product.description,
+        defaultPriceId: price.id,
       }
     },
     revalidate: 60 * 60 * 1, // 1hour
--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,17 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Product() {
   return (
-    <h1>Success</h1>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer></ImageContainer>
+      <p>Uhull <strong>Kened Felix</strong>, sua <strong>camiseta Beyond the Limits</strong> já esta a caminho.</p>
+
+      <Link href="/">
+        Voltar ao catalogo
+      </Link>
+    </SuccessContainer>
   )
 }
